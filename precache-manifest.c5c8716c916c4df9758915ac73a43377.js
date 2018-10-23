c6f21a34393de24be97a13618dcb04e916a1a446
ajz003 <ajz003@ucla.edu>
2018-10-23 14:49:20 -0700

    Updates

---
+++ precache-manifest.c5c8716c916c4df9758915ac73a43377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "98bf31c68fee7b33c915",
+    "url": "/memory-game/static/js/main.98bf31c6.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/memory-game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "98bf31c68fee7b33c915",
+    "url": "/memory-game/static/css/main.333857cc.chunk.css"
+  },
+  {
+    "revision": "ff591b89d2976a4b1c412bdbf84f420d",
+    "url": "/memory-game/index.html"
+  }
+];
